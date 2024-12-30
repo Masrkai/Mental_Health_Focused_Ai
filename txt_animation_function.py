d0e6b6bf1895c23a84efd6510672f8c3307f30c9
Masrkai <118424417+Masrkai@users.noreply.github.com>
2024-12-30 03:40:26 +0200

    Update txt_animation_function.py

--- txt_animation_function.py
+++ txt_animation_function.py
@@ -1,10 +1,16 @@
-import time
-import streamlit as st
+import time  # For introducing delays
+import streamlit as st  # Streamlit for building interactive web applications
 
 # Function to print animated text in Streamlit
 def print_animated_txt(text):
-    placeholder = st.empty()  
-    for char in text:
-        placeholder.markdown(char, unsafe_allow_html=True)  
-        time.sleep(0.05)  
-    placeholder.markdown(text, unsafe_allow_html=True) 
+    """
+    Displays an animated text effect in a Streamlit app, showing one character at a time.
+
+    Parameters:
+    text (str): The text to display in an animated fashion.
+    """
+    placeholder = st.empty()  # Create a placeholder for dynamic content updates
+    for char in text:  # Iterate through each character in the provided text
+        placeholder.markdown(char, unsafe_allow_html=True)  # Display the character in the placeholder
+        time.sleep(0.05)  # Pause briefly before displaying the next character
+    placeholder.markdown(text, unsafe_allow_html=True)  # Display the full text after the animation
